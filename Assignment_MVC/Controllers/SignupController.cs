6d1e182c83c55b2e7e857a058dfe50892e160c48
PRADEEP <PRADEEP@LAPTOP-6FQVFC4N>
2020-02-12 05:10:06 -0800

    Add project files.

---
+++ Assignment_MVC/Controllers/SignupController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment_MVC.Controllers
+{
+    public class SignupController : Controller
+    {
+        // GET: Signup
+        public ActionResult PageSignup()
+        {
+            return View();
+        }
+    }
+}
